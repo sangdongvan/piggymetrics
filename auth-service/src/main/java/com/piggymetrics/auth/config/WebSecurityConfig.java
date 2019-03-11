5c90204412a1570c47b762cc0a864bc864cc3b5c
Sang Dong <sang.dongvan@asnet.com.vn>
2019-03-11 18:26:02 +0700

    Workaround to address issue can't whitelist the actuator endpoints of auth-service

--- auth-service/src/main/java/com/piggymetrics/auth/config/WebSecurityConfig.java
+++ auth-service/src/main/java/com/piggymetrics/auth/config/WebSecurityConfig.java
@@ -4,10 +4,8 @@ import com.piggymetrics.auth.service.security.MongoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,23 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @author cdov
  */
 @Configuration
-@Order(2) // before the server resource configuration to avoid protecting on actuator endpoints
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MongoUserDetailsService userDetailsService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-                .authorizeRequests()
-                .antMatchers("/actuator/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-        // @formatter:on
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
