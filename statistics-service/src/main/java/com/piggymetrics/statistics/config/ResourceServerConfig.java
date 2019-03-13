e4ebbca623024b893b04058bd00ef8b422800db4
Sang Dong <sang.dongvan@asnet.com.vn>
2019-03-13 09:47:07 +0700

    make unique instance id for service registered on consul

--- statistics-service/src/main/java/com/piggymetrics/statistics/config/ResourceServerConfig.java
+++ statistics-service/src/main/java/com/piggymetrics/statistics/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
@@ -15,11 +16,22 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @EnableResourceServer
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
     @Autowired
     private ResourceServerProperties sso;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+    }
 
     @Bean
     public ResourceServerTokenServices tokenServices() {
         return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
     }
+
 }
